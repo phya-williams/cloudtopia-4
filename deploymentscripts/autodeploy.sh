3ad6f581e1e45b1d6101624700a72d4692a136a0
phya-williams <phya.williams@gmail.com>
2025-06-13 15:22:34 -0400

    Update autodeploy.sh

--- deploymentscripts/autodeploy.sh
+++ deploymentscripts/autodeploy.sh
@@ -1,7 +1,7 @@
 export RESOURCE_GROUP=$(az group list --query "[0].name" -o tsv)
 git clone https://github.com/phya-williams/cloudtopia-4
 cd cloudtopia-4
+cd deploymentscripts
 chmod +x deploy.sh
-chmod +x create-alerts.sh
 ./deploy.sh
-./create-alerts.sh
+
